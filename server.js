c4b69891e2abf3ba080159c7a7e5d2bd6f319fc4
Ahmed Dghaies <dghais.ahmed7@gmail.com>
2020-11-19 00:34:17 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to cynoia application." });
+});
+
+
+const PORT = process.env.PORT || 8080;
+var server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
